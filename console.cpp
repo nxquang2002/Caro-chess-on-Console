c4fd1e85fb610650796bea0d5ef3384dab264de8
nxquang2002apcs <nxquang20@apcs.com>
2021-05-23 21:08:28 +0700

    Các hàm quản lý cửa sổ console

---
+++ console.cpp
@@ -0,0 +1,146 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include "console.h"
+
+
+int inputKey()
+{
+	if (_kbhit())
+	{
+		int key = _getch();
+
+		if (key == 224)	// special key
+		{
+			key = _getch();
+			return key + 1000;
+		}
+
+		return key;
+	}
+	else
+	{
+		return key_none;
+	}
+
+	return key_none;
+}
+
+
+//-------------------------Screen-------------------------
+void clrscr()
+{
+	CONSOLE_SCREEN_BUFFER_INFO	csbiInfo;                  
+	HANDLE	hConsoleOut;
+	COORD	Home = {0,0};
+	DWORD	dummy;
+
+	hConsoleOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	GetConsoleScreenBufferInfo(hConsoleOut,&csbiInfo);
+
+	FillConsoleOutputCharacter(hConsoleOut,' ',csbiInfo.dwSize.X * csbiInfo.dwSize.Y,Home,&dummy);
+	csbiInfo.dwCursorPosition.X = 0;
+	csbiInfo.dwCursorPosition.Y = 0;
+	SetConsoleCursorPosition(hConsoleOut,csbiInfo.dwCursorPosition);
+}
+
+
+//screen: goto [x,y]
+void gotoXY (int column, int line)
+{
+	COORD coord;
+	coord.X = column;
+	coord.Y = line;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+}
+
+
+//screen: get [x]
+int whereX()
+{
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	if(GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
+		return csbi.dwCursorPosition.X;
+	return -1;
+}
+
+
+//screen: get [y]
+int whereY()
+{
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	if(GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
+		return csbi.dwCursorPosition.Y;
+	return -1;
+}
+
+
+void TextColor (int color)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE) , color);
+}
+
+//Hàm ẩn con trỏ trên màn hình Console
+void Nocursortype()
+{
+	CONSOLE_CURSOR_INFO Info;
+	Info.bVisible = FALSE;
+	Info.dwSize = 20;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &Info);
+}
+
+//Hiện con trỏ chuột trở lại trên màn hình
+void UnNocursortype()
+{
+	CONSOLE_CURSOR_INFO Info;
+	Info.bVisible = TRUE;
+	Info.dwSize = 20;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &Info);
+}
+
+//Hàm vô hiệu hóa thay đổi kích thước màn hình Console
+void DisableResizeWindow()
+{
+	HWND hWnd = GetConsoleWindow();
+	SetWindowLong(hWnd, GWL_STYLE, GetWindowLong(hWnd, GWL_STYLE) & ~WS_SIZEBOX);
+}
+
+//Hàm vô hiệu hóa các nút Minimize, Maximize và Close
+void DisableCtrButton(bool Close, bool Min, bool Max)
+{
+	HWND hWnd = GetConsoleWindow();
+	HMENU hMenu = GetSystemMenu(hWnd, false);
+
+	if (Close == 1)
+	{
+		DeleteMenu(hMenu, SC_CLOSE, MF_BYCOMMAND);
+	}
+	if (Min == 1)
+	{
+		DeleteMenu(hMenu, SC_MINIMIZE, MF_BYCOMMAND);
+	}
+	if (Max == 1)
+	{
+		DeleteMenu(hMenu, SC_MAXIMIZE, MF_BYCOMMAND);
+	}
+}
+
+//Thay đổi kích thước Console
+void SetWindowSize(SHORT width, SHORT height)
+{
+	HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	SMALL_RECT WindowSize;
+	WindowSize.Top = 0;
+	WindowSize.Left = 0;
+	WindowSize.Right = width;
+	WindowSize.Bottom = height;
+
+	SetConsoleWindowInfo(hStdout, 1, &WindowSize);
+}
+
+//Ẩn hiện thanh cuốn 
+void ShowScrollbar(BOOL Show)
+{
+	HWND hWnd = GetConsoleWindow();
+	ShowScrollBar(hWnd, SB_BOTH, Show);
+}
